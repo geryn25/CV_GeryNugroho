1c37102e285013123b2017b957edcbfb37d0c60e
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-02-23 06:58:42 +0700

    Sidedar Finish
    sidebar jadi

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -3,14 +3,20 @@ $(document).ready(function(){
         $("#leftNav").css("transition","1s");
         $("#leftNav").css("display","inline-block");
         $("#leftNav").css("width","200px");
-        $("#openNav").css("display","none")
+        $("#main").css("margin-left","210px");
+        
+        
+        $("body").css("background-color","rgba(0,0,0,0.4)");
+        
         
     });
     $(".closebtn").click(function() {
         $("#leftNav").css("transition","1s");
         $("#leftNav").css("width","0px");
-        $("#openNav").css("display","inline-block")
+        $("#main").css("margin-left","10px");
+        $("body").css("background-color","white");
         
     });
+    
  });
  
