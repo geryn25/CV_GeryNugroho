ac3965032c5bda61cbb1358c7d2791ee57065b18
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-02-23 09:36:01 +0700

    Full Page
    full page

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,9 +1,11 @@
+
 $(document).ready(function(){
     $("#openNav").click(function() {
         $("#leftNav").css("transition","1s");
         $("#leftNav").css("display","inline-block");
         $("#leftNav").css("width","200px");
-        $("#main").css("margin-left","210px");
+        $(".main").css("margin-left","200px");
+        $("#judul").css("margin-left","40%");
         
         
         $("body").css("background-color","rgba(0,0,0,0.4)");
@@ -13,10 +15,13 @@ $(document).ready(function(){
     $(".closebtn").click(function() {
         $("#leftNav").css("transition","1s");
         $("#leftNav").css("width","0px");
-        $("#main").css("margin-left","10px");
+        $(".main").css("margin-left","0px");
         $("body").css("background-color","white");
         
     });
     
+    
  });
+
+
  
